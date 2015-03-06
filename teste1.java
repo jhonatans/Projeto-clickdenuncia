e8ac8257c7add48fe870a91639a19063fdb350db
icarosantos <projetoclickdenuncia@outlook.com>
2015-03-06 19:30:30 -0300

    Test for Click Denuncia
    A test.

---
+++ teste1.java
@@ -0,0 +1,23 @@
+package Teste;
+
+public class teste1 {
+
+	static String hello = "Hello World!";
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println(hello);	
+		
+	
+		
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+	
+
+}
